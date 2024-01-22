b6ce71e0b945c9a075b2177326909ce4cdd9f2cd
rithiklogu <bazeer33333@gmail.com.com>
2024-01-22 16:50:53 +0530

    logger added

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-from src.Wine_QT_Prediction.config import configuration 
+from src.Wine_QT_Prediction import logger
+
+logger.info("this is Rithivi...!")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "wine_quality_prediction"
 AUTHOR_USER_NAME = "rithiklogu"
